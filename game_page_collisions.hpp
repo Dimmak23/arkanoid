8ede1d9e0fafe284a0364471d2a11132e2a6c58e
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2022-10-17 23:36:03 +0300

    for better physics

--- game_page_collisions.hpp
+++ game_page_collisions.hpp
@@ -39,6 +39,7 @@
 
 
 
+
 //DEFINES
 #define BLOCK_TOP			dynamics.conveyor.at(indexer).getGlobalBounds().top
 #define BLOCK_BOTTOM		dynamics.conveyor.at(indexer).getGlobalBounds().top + dynamics.conveyor.at(indexer).getGlobalBounds().height
@@ -61,6 +62,15 @@
 //#define ITER_LEFT			test.getGlobalBounds().left
 //#define ITER_RIGHT			test.getGlobalBounds().left + test.getGlobalBounds().width
 //#define ITER_WIDTH			test.getGlobalBounds().width
+
+#define UNIT_TOP			unit.getGlobalBounds().top
+#define UNIT_LEFT			unit.getGlobalBounds().left
+#define UNIT_HEIGHT			unit.getGlobalBounds().height
+#define UNIT_WIDTH			unit.getGlobalBounds().width
+#define UNIT_BOTTOM			UNIT_TOP + UNIT_HEIGHT
+#define UNIT_RIGHT			UNIT_LEFT + UNIT_WIDTH
+#define U_HOR_TOUCH(coor)	(UNIT_LEFT < coor) && (coor < UNIT_RIGHT)
+#define U_VER_TOUCH(coor)	(UNIT_BOTTOM > coor) && (coor > UNIT_TOP)
 
 #define BALL_TOP			dynamics.ball->getGlobalBounds().top
 #define BALL_BOTTOM			dynamics.ball->getGlobalBounds().top + dynamics.ball->getGlobalBounds().height
@@ -120,6 +130,78 @@ inline static void checkLostBall(Game::dynamicUnits& dynamics)
 	}
 }
 
+inline static colission_detection checkCornerCollision(
+	const float& corner_X,
+	const float& corner_Y,
+	const float& centerX_BLL,
+	const float& centerY_BLL,
+	const float& rad_BLL
+)
+{
+	colission_detection result;
+
+	float cathetus_sqr_x = to_f(pow((centerX_BLL - corner_X), 2));
+	float cathetus_sqr_y = to_f(pow((centerY_BLL - corner_Y), 2));
+	float hypotenuse = sqrt(cathetus_sqr_x + cathetus_sqr_y);
+
+	if (hypotenuse < rad_BLL) result.diagnose = true;
+	else
+	{
+		///DON'T GO FURTHER IF THERE IS NO INTERSECTION
+
+		result.diagnose = false;
+		result.vector = sf::Vector2f(0, 0);
+		return result;
+	}
+
+	//Object falling down with angle (in radian units):
+	float angle = std::acos((std::abs(centerX_BLL - corner_X)) / hypotenuse);
+	float tan_alpha{ std::tan(angle) };
+
+	//Use coupling to avoid division by zero
+	//This is possible if ball go thru RIGHT side of the PADDLE,
+	//or wise versa,
+	//with '0' angle of the SPEED to the ground
+	if (std::tan(angle) < 0.01f) tan_alpha = 0.01f;
+
+	//Cathetus by X axis of the intersection:
+	//(this is always a positive value)
+	float move_x{ (rad_BLL - hypotenuse) / (std::tan(angle)) };
+
+	//If ball go from the LEFT it should be moved to the LEFT
+	if (centerX_BLL < corner_X) move_x *= -1.f;
+	//positive movement: to the RIGHT
+
+	//USE COUPLING for the X axis, make sure we don't throw ball outside the field
+
+	if ((centerX_BLL + rad_BLL + move_x) > (game_field.origin_x + game_field.overall_width))
+	{
+		move_x = game_field.origin_x + game_field.overall_width - centerX_BLL - rad_BLL;
+	}
+	else if ((centerX_BLL - rad_BLL + move_x) < game_field.origin_x)
+	{
+		move_x = -(centerX_BLL - rad_BLL - game_field.origin_x);
+	}
+
+	///GLITCH (BALL GOES TO FAR FROM PADDLE) SOLUTION
+	if (move_x > rad_BLL) move_x = rad_BLL;
+	else if (move_x < -rad_BLL) move_x = -rad_BLL;
+
+	//We are always go UP from the paddle
+	//Cathetus by Y axis of the intersection:
+	//always negative, cause will be bounced back - UP
+	float move_y = -(rad_BLL - hypotenuse) * (std::tan(angle));
+
+	///GLITCH (BALL GOES TO FAR FROM PADDLE) SOLUTION
+	if (move_y > rad_BLL) move_y = rad_BLL;
+	else if (move_y < -rad_BLL) move_y = -rad_BLL;
+
+	//So now we can setup a proper vector to the BALL movement
+	result.vector = sf::Vector2f(move_x, move_y);
+
+	return result;
+}
+
 inline static colission_detection checkCircleCollision(
 														const float& centerX_PDL,
 														const float& centerY_PDL,
@@ -157,7 +239,7 @@ inline static colission_detection checkCircleCollision(
 
 	//Cathetus by X axis of the intersection:
 	//(this is always a positive value)
-	float move_x{ move_x = (rad_PDL + rad_BLL - hypotenuse) / (std::tan(angle)) };
+	float move_x{ (rad_PDL + rad_BLL - hypotenuse) / (std::tan(angle)) };
 
 	//If ball go from the LEFT it should be moved to the LEFT
 	if (centerX_BLL < centerX_PDL) move_x *= -1.f;
@@ -674,8 +756,35 @@ inline static void collideUnits(Game::dynamicUnits& dynamics, const float& d_tim
 
 }
 
+//static inline bool one_timer_b{ true };
+//static inline bool one_timer_a{ true };
+
 inline static void hitUnits(Game::dynamicUnits& dynamics, const float& d_time)
 {
+	//if(one_timer_b)
+	//{
+	//	for (int index{}; index < dynamics.conveyor_map.size(); index++)
+	//	{
+	//		if (dynamics.conveyor_map.at(index).block)
+	//		{
+	//			if (one_timer_b)
+	//			{
+	//				std::cout << "Block width = " << dynamics.conveyor.at(index).getGlobalBounds().width << '\n';
+	//				std::cout << "Block height = " << dynamics.conveyor.at(index).getGlobalBounds().height << '\n';
+	//			}
+	//			one_timer_b = false;
+	//			continue;
+	//		}
+
+	//		if(one_timer_a)
+	//		{
+	//			std::cout << "Ability width = " << dynamics.conveyor.at(index).getGlobalBounds().width << '\n';
+	//			std::cout << "Ability height = " << dynamics.conveyor.at(index).getGlobalBounds().height << '\n';
+	//			one_timer_a = false;
+	//		}
+	//	}
+	//}
+	
 
 /////////////////////////////////////BALL VS CONVEYOR BELT//////////////////////////////////////////
 
@@ -688,93 +797,150 @@ inline static void hitUnits(Game::dynamicUnits& dynamics, const float& d_time)
 	std::vector<sf::Sprite>::iterator eraser_unit;
 	std::vector<Game::block_map_variables>::iterator eraser_mapper;
 
-	for (int indexer{}; indexer < dynamics.conveyor.size(); )
+	//for (int indexer{}; indexer < dynamics.conveyor.size(); )
+	//{
+	//	if (
+	//			(
+	//				//LEFT side of the BALL between BLOCK bounds
+	//				(BLOCK_LEFT < BALL_LEFT) && (BALL_LEFT < BLOCK_RIGHT)
+	//				||
+	//				//OR
+	//				//RIGHT side of the BALL between BLOCK bounds
+	//				(BLOCK_LEFT < BALL_RIGHT) && (BALL_RIGHT < BLOCK_RIGHT)
+	//			)
+	//		&&
+	//			(
+	//				//TOP of the BALL between BLOCK bounds
+	//				(BLOCK_TOP < BALL_TOP) && (BALL_TOP < BLOCK_BOTTOM)
+	//				||
+	//				//OR
+	//				//BOTTOM of the BALL between BLOCK bounds
+	//				(BLOCK_TOP < BALL_BOTTOM) && (BALL_BOTTOM < BLOCK_BOTTOM)
+	//			)
+	//		)
+	//	{
+
+	//		if ( std::abs(bll(V_X)) > std::abs(bll(V_Y)) )
+	//		{
+	//			//Y axis acceleration speed will be inverted
+	//			bll(A_Y) *= -1.f;
+
+	//			//Bouncing from the block
+	//			bll(V_X) *= Game::dynamicUnits::bll_bounce;
+	//			bll(V_Y) *= -Game::dynamicUnits::bll_bounce;
+	//		}
+	//		else if ( std::abs(bll(V_X)) < std::abs(bll(V_Y)) )
+	//		{
+	//			//X axis acceleration and speed will be inverted
+	//			bll(A_X) *= -1.f;
+
+	//			//Bouncing from the block
+	//			bll(V_X) *= -Game::dynamicUnits::bll_bounce;
+	//			bll(V_Y) *= Game::dynamicUnits::bll_bounce;
+	//		}
+	//		else
+	//		{
+	//			//BOTH axis accelerations and speeds will be inverted
+	//			bll(A_X) *= -1.f;
+	//			bll(A_Y) *= -1.f;
+
+	//			//Bouncing from the block
+	//			bll(V_Y) *= -Game::dynamicUnits::bll_bounce;
+	//			bll(V_Y) *= Game::dynamicUnits::bll_bounce;
+	//		}
+
+	//		//coordinates re-calculation
+	//		bll(DELTA_X) = bll(V_X) * d_time + bll(A_X) * d_time * d_time / 2.f;
+	//		bll(DELTA_Y) = bll(V_Y) * d_time + bll(A_Y) * d_time * d_time / 2.f;
+
+	//		//Take deltas from kinematic container and move the ball
+	//		dynamics.ball->move(sf::Vector2f(bll(DELTA_X), bll(DELTA_Y)));
+
+	//		//If it's a block, we increase score and erase it then
+	//		if (dynamics.conveyor_map.at(indexer).block)
+	//		{
+	//			//Choose function to execute
+	//			executor do_this{ dynamics.conveyor_map.at(indexer).function};
+
+	//			//Pass variable to that function
+	//			do_this(dynamics.conveyor_map.at(indexer).operand);
+
+	//			//AND of course destroy such block
+	//			eraser_unit = dynamics.conveyor.begin() + indexer;
+
+	//			//Also destory a mapper for such block
+	//			eraser_mapper = dynamics.conveyor_map.begin() + indexer;
+	//			
+	//			dynamics.conveyor_map.erase(eraser_mapper);
+	//			dynamics.conveyor.erase(eraser_unit);
+
+	//		}
+	//		//If it's an ability: bounce from it
+	//		else
+	//		{
+	//		
+	//		}
+
+	//		//NOT necassary to traverse the hall container
+	//		//if one hitted already we need to find another one in the next frame
+	//		//return;
+	//	}
+	//	///NOT SURE ABOUT THIS
+	//	//IF we erase some block: indexer will be lead to the next one already
+	//	//BUT! if we didn't delete anything: we will increment position in the container
+	//	else indexer++;
+	//}
+
+	//Seed for only blocks
+	auto only_blocks = [](const Game::block_map_variables& struc)
 	{
-		if (
-				(
-					//LEFT side of the BALL between BLOCK bounds
-					(BLOCK_LEFT < BALL_LEFT) && (BALL_LEFT < BLOCK_RIGHT)
-					||
-					//OR
-					//RIGHT side of the BALL between BLOCK bounds
-					(BLOCK_LEFT < BALL_RIGHT) && (BALL_RIGHT < BLOCK_RIGHT)
-				)
-			&&
-				(
-					//TOP of the BALL between BLOCK bounds
-					(BLOCK_TOP < BALL_TOP) && (BALL_TOP < BLOCK_BOTTOM)
-					||
-					//OR
-					//BOTTOM of the BALL between BLOCK bounds
-					(BLOCK_TOP < BALL_BOTTOM) && (BALL_BOTTOM < BLOCK_BOTTOM)
-				)
-			)
+		return struc.block;
+	};
+
+
+	//If there is gonna be a collision?
+	auto bounds_collision = [&](const sf::Sprite& unit)
+	{
+		return
+			(
+			(U_HOR_TOUCH(BALL_LEFT) && (U_VER_TOUCH(BALL_TOP) || U_VER_TOUCH(BALL_BOTTOM)))
+			||
+			(U_HOR_TOUCH(BALL_RIGHT) && (U_VER_TOUCH(BALL_TOP) || U_VER_TOUCH(BALL_BOTTOM)))
+			);
+	};
+	
+	int map_adder{};
+
+	auto attacked_unit = dynamics.conveyor.begin();
+	
+	while(attacked_unit != dynamics.conveyor.end())
+	{
+		auto give_block = std::ranges::find_if(dynamics.conveyor_map.begin() + map_adder, dynamics.conveyor_map.end(), only_blocks);
+	
+		if (give_block != dynamics.conveyor_map.end())
 		{
-
-			if ( std::abs(bll(V_X)) > std::abs(bll(V_Y)) )
-			{
-				//Y axis acceleration speed will be inverted
-				bll(A_Y) *= -1.f;
-
-				//Bouncing from the block
-				bll(V_X) *= Game::dynamicUnits::bll_bounce;
-				bll(V_Y) *= -Game::dynamicUnits::bll_bounce;
-			}
-			else if ( std::abs(bll(V_X)) < std::abs(bll(V_Y)) )
-			{
-				//X axis acceleration and speed will be inverted
-				bll(A_X) *= -1.f;
-
-				//Bouncing from the block
-				bll(V_X) *= -Game::dynamicUnits::bll_bounce;
-				bll(V_Y) *= Game::dynamicUnits::bll_bounce;
-			}
-			else
-			{
-				//BOTH axis accelerations and speeds will be inverted
-				bll(A_X) *= -1.f;
-				bll(A_Y) *= -1.f;
-
-				//Bouncing from the block
-				bll(V_Y) *= -Game::dynamicUnits::bll_bounce;
-				bll(V_Y) *= Game::dynamicUnits::bll_bounce;
-			}
-
-			//coordinates re-calculation
-			bll(DELTA_X) = bll(V_X) * d_time + bll(A_X) * d_time * d_time / 2.f;
-			bll(DELTA_Y) = bll(V_Y) * d_time + bll(A_Y) * d_time * d_time / 2.f;
-
-			//Take deltas from kinematic container and move the ball
-			dynamics.ball->move(sf::Vector2f(bll(DELTA_X), bll(DELTA_Y)));
-
-			//If it's a block, we increase score and erase it then
-			if (dynamics.conveyor_map.at(indexer).block)
-			{
-				//Choose function to execute
-				executor do_this{ dynamics.conveyor_map.at(indexer).function};
-
-				//Pass variable to that function
-				do_this(dynamics.conveyor_map.at(indexer).operand);
-
-				//AND of course destroy such block
-				eraser_unit = dynamics.conveyor.begin() + indexer;
-
-				//Also destory a mapper for such block
-				eraser_mapper = dynamics.conveyor_map.begin() + indexer;
-				
-				dynamics.conveyor_map.erase(eraser_mapper);
-				dynamics.conveyor.erase(eraser_unit);
-
-			}
-
-			//NOT necassary to traverse the hall container
-			//if one hitted already we need to find another one in the next frame
-			//return;
+			attacked_unit = dynamics.conveyor.begin() + (give_block - dynamics.conveyor_map.begin());
+			map_adder = give_block - dynamics.conveyor_map.begin() + 1;
 		}
-		///NOT SURE ABOUT THIS
-		//IF we erase some block: indexer will be lead to the next one already
-		//BUT! if we didn't delete anything: we will increment position in the container
-		else indexer++;
+		else attacked_unit = dynamics.conveyor.end();
+
+		auto attacked_block = std::ranges::find_if(attacked_unit, dynamics.conveyor.end(), bounds_collision);
+
+#define BALL_UL_CORNER ()&&()
+
+		//If corner
+		if(
+			()
+		)
+		{
+		
+		}
+
+
+		//If flat surface
+
 	}
+
+
 
 }
